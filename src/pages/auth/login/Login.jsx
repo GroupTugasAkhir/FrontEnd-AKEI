8174dfb3ffb49f5a219d7e101e4cef523a0db857
anggi <anggiprastianto30@gmail.com>
2020-11-30 11:57:13 +0700

    final auth before backend
    adding react-reveal library, fixing sign up and sign in design

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,13 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './logstyle.css'
 import Authimg from '../../../assets/authimg.jpg'
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import Bounce from 'react-reveal/Bounce';
 
 const Login = () => {
     const [seePass, setseePass] = useState(false)
+    const [user, setuser] = useState('')
+    const [pass, setpass] = useState('')
+    const [errorinfo, seterrorinfo] = useState('')
+
+    useEffect(()=> {
+
+    },[])
+
+    const onUsernameChange = (e) => {
+        if(e.target.value) {
+            setuser(e.target.value)
+            seterrorinfo('')
+        } else {
+            seterrorinfo('')
+        }
+    }
+
+    const onPasswordChange = (e) => {
+        if(e.target.value) {
+            setpass(e.target.value)
+            seterrorinfo('')
+        } else {
+            seterrorinfo('')
+        }
+    }
+
+    const onSignIn = () => {
+        if(user & pass) {
+            console.log('user and pass');
+        } else {
+            seterrorinfo('need password & username')
+        }
+    }
 
     return (
         <>
-            <div className='screen'>
+            <div className='screenlog'>
                 <div className='log-left-side'>
                     <div className='addon' >
                         
@@ -15,33 +51,51 @@ const Login = () => {
                     <div className='addon2' >
                         
                     </div>
-                    <div className="auth-title-box">
-                        <h2 className='maintext'>Sign In to <br/>Recharge Direct</h2>
-                        <p>If you don't have an account <br/> You can &nbsp; &nbsp; <span style={{color:'#C3DCD6', fontWeight: 'bolder', cursor: 'pointer'}}>Register here!</span> </p>
-                    </div>
-                    <div className='authimg' >
-                        <img height='100%' width='100%' style={{objectFit: 'cover', objectPosition: 'bottom'}} src={Authimg} alt="authimg"/>
-                    </div>
+                    <Bounce>
+                        <div className="log-title-box">
+                            <h2 className='maintext'>Sign In to <br/>Recharge Direct</h2>
+                            <p>If you don't have an account <br/> You can &nbsp; &nbsp; <span style={{color:'#C3DCD6', fontWeight: 'bolder', cursor: 'pointer'}}>Register here!</span> </p>
+                        </div>
+                    </Bounce>
+                    <Zoom cascade>
+                        <div className='logimg' >
+                            <img height='100%' width='100%' style={{objectFit: 'cover', objectPosition: 'bottom'}} src={Authimg} alt="authimg"/>
+                        </div>
+                    </Zoom>
                 </div>
                 <div className='log-right-side'>
-                    <div className='login-form'>
-                        <input className='mt-3' type="text" placeholder='Enter username' />
-                        <input type={seePass?"text":"password"} placeholder='Password' />
-                        {
-                            seePass?
-                            <button className='seepassbut mb-4' onClick={()=>setseePass(false)} >
-                                <i class="far fa-eye"></i>
-                            </button>
-                            :
-                            <button className='seepassbut mb-4' onClick={()=>setseePass(true)}>
-                                <i class="far fa-eye-slash"></i>
-                            </button>
+                    <Fade right>
+                        <div className='login-form'>
+                            <input className='mt-3' type="text" placeholder='Enter username' onChange={onUsernameChange} />
+                            <input type={seePass?"text":"password"} placeholder='Password' onChange={onPasswordChange} />
+                            {
+                                seePass?
+                                <button className='seepassbut mb-4' onClick={()=>setseePass(false)} >
+                                    <i className="far fa-eye"></i>
+                                </button>
+                                :
+                                <button className='seepassbut mb-4' onClick={()=>setseePass(true)}>
+                                    <i className="far fa-eye-slash"></i>
+                                </button>
 
-                        }
-                        <button className='signin-button' >
-                            Sign In
-                        </button>
-                    </div>
+                            }
+                            <div style={{width: '270px'}}>
+                                {
+                                    errorinfo?
+                                    <Fade top>
+                                        <div className='alert alert-danger'>
+                                            {errorinfo} <span className='logerrorstyle' onClick={()=>seterrorinfo('')}>X</span>
+                                        </div>
+                                    </Fade>
+                                    :
+                                    null
+                                }
+                            </div>
+                            <button className='signin-button' onClick={onSignIn} >
+                                Sign In
+                            </button>
+                        </div>
+                    </Fade>
                 </div>
             </div>
         </>
--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -1,54 +1,150 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './regstyle.css'
 import Mainregimg from '../../../assets/mainregimg.jpg'
 import Secregimg from '../../../assets/secregimg.jpg'
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import Bounce from 'react-reveal/Bounce';
 
 const Register = () => {
     const [seePass, setseePass] = useState(false)
+    const [user, setuser] = useState('')
+    const [email, setemail] = useState('')
+    const [pass, setpass] = useState('')
+    const [passconfirm, setpassconfirm] = useState('')
+    const [errorinfo, seterrorinfo] = useState('')
+    const [confirminfo, setconfirminfo] = useState('')
+
+    useEffect(()=> {
+
+    },[])
+
+    const onUsernameChange = (e) => {
+        if(e.target.value) {
+            setuser(e.target.value)
+            seterrorinfo('')
+        } else if(e.target.value === '') {
+            seterrorinfo('Username is required!')
+        }
+    }
+
+    const onEmailChange = (e) => {
+        var emailtester = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g).test(e.target.value)
+        if(emailtester) {
+            setemail(e.target.value)
+            seterrorinfo('')
+        } else if (e.target.value === '') {
+            seterrorinfo('')
+        } else {
+            seterrorinfo('Email is not in a valid pattern')
+        }
+    }
+
+    const onPasswordChange = (e) => {
+        var reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,32}$/
+        var passwordtester = reg.test(e.target.value)
+
+        if(passwordtester) {
+            setpass(e.target.value)
+            seterrorinfo('')
+        } else if(e.target.value === '') {
+            seterrorinfo('')
+        } else {
+            seterrorinfo('Must Contain 6 Characters, One Uppercase, One Lowercase, and One Number')
+        }
+    }
+
+    const onPasswordConfirm = (e) => {
+        if(e.target.value === pass) {
+            setpassconfirm(e.target.value)
+            seterrorinfo('')
+            setconfirminfo('Password is match')
+        } else if(e.target.value === '') {
+            seterrorinfo('')
+            setconfirminfo('')
+        } else {
+            seterrorinfo('Password is not match!')
+            setconfirminfo('')
+        }
+    }
+
+    const onSignUp = () => {
+        console.log(user);
+        console.log(email);
+        console.log(passconfirm);
+    }
 
     return (
         <>
             <div className='screen'>
                 <div className='reg-left-side'>
-                    <div className='register-form'>
-                        <input className='mt-3' type="text" placeholder='Enter username' />
-                        <input className='mt-2' type="text" placeholder='Enter email' />
-                        <input type={seePass?"text":"password"} placeholder='Password' />
-                        <input type={seePass?"text":"password"} placeholder='Confirm Password' />
-                        {
-                            seePass?
-                            <button className='seepassbut mb-4' onClick={()=>setseePass(false)} >
-                                <i class="far fa-eye"></i>
-                            </button>
-                            :
-                            <button className='seepassbut mb-4' onClick={()=>setseePass(true)}>
-                                <i class="far fa-eye-slash"></i>
-                            </button>
+                    <Fade left cascade>
+                        <div className='register-form'>
+                            <input className='mt-3' type="text" placeholder='Enter username' onChange={onUsernameChange} />
+                            <input className='mt-2' type="text" placeholder='Enter email' onChange={onEmailChange} />
+                            <input type={seePass?"text":"password"} placeholder='Password' onChange={onPasswordChange} />
+                            <input type={seePass?"text":"password"} placeholder='Confirm Password' onChange={onPasswordConfirm} />
+                            {
+                                seePass?
+                                <button className='seepassbut mb-4' onClick={()=>setseePass(false)} >
+                                    <i className="far fa-eye"></i>
+                                </button>
+                                :
+                                <button className='seepassbut mb-4' onClick={()=>setseePass(true)}>
+                                    <i className="far fa-eye-slash"></i>
+                                </button>
 
-                        }
-                        <button className='signup-button' >
-                            Sign Up
-                        </button>
-                    </div>
+                            }
+                            <div style={{width: '270px'}}>
+                                {
+                                    errorinfo?
+                                    <Fade top>
+                                        <div className='alert alert-danger'>
+                                            {errorinfo} <span className='errorstyle' onClick={()=>seterrorinfo('')}>X</span>
+                                        </div>
+                                    </Fade>
+                                    :
+                                    null
+                                }
+                                {
+                                    confirminfo?
+                                    <Fade top>
+                                        <div className='alert alert-success'>
+                                            {confirminfo} <span className='errorstyle' onClick={()=>setconfirminfo('')} >X</span>
+                                        </div>
+                                    </Fade>
+                                    :
+                                    null
+                                }
+                            </div>
+                            <button className='signup-button' onClick={onSignUp} >
+                                Sign Up
+                            </button>
+                        </div>
+                    </Fade>
                 </div>
                 <div className='reg-right-side'>
-                    <div className='addon' >
+                    <div className='addon3' >
                     
                     </div>
-                    <div className='addon2' >
+                    <div className='addon4' >
                         
                     </div>
-                    <div className="auth-title-box">
-                        <h2 className='maintext'>Sign Up Now!</h2>
-                        <p>In order to experience <br/>New <span style={{color:'green', fontWeight: 'bolder'}}>environment</span> at your place </p>
-                        
-                    </div>
-                    <div className='secregimg' >
-                        <img height='100%' width='100%' src={Secregimg} alt="secregimg"/>
-                    </div>
-                    <div className='mainregimg' >
-                        <img height='100%' width='100%' src={Mainregimg} alt="mainregimg"/>
-                    </div>
+                    <Bounce>
+                        <div className="auth-title-box">
+                            <h2 className='maintext'>Sign Up Now!</h2>
+                            <p>In order to experience <br/>New <span style={{color:'green', fontWeight: 'bolder'}}>environment</span> at your place </p>
+                            
+                        </div>
+                    </Bounce>
+                    <Zoom cascade>
+                        <div className='secregimg' >
+                            <img height='100%' width='100%' src={Secregimg} alt="secregimg"/>
+                        </div>
+                        <div className='mainregimg' >
+                            <img height='100%' width='100%' src={Mainregimg} alt="mainregimg"/>
+                        </div>
+                    </Zoom>
                 </div>
             </div>
         </>
