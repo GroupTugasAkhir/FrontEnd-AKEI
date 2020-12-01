33101cce7b8fd62986e12fb08028bf1edd50246e
Hannah <hannah4669@users.noreply.github.com>
2020-12-02 00:38:07 +0700

    aa

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -7,21 +7,88 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 
+const AntTabs = withStyles({
+    root: {
+      borderBottom: '0px solid #e8e8e8',
+    },
+    indicator: {
+      backgroundColor: 'coral',
+    },
+})(Tabs);
+  
+const AntTab = withStyles((theme) => ({
+    root: {
+      textTransform: 'none',
+      minWidth: 72,
+      fontWeight: theme.typography.fontWeightRegular,
+      marginRight: theme.spacing(4),
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:hover': {
+        opacity: 1,
+      },
+      '&$selected': {
+        color: 'coral',
+        fontWeight: theme.typography.fontWeightMedium,
+      },
+      '&:focus': {
+        color: 'coral',
+      },
+    },
+    selected: {},
+}))((props) => <Tab disableRipple {...props} />);
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    padding: {
+      padding: theme.spacing(1),
+    },
+    demo1: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    demo2: {
+      backgroundColor: '#2e1534',
+    },
+}));
 
 const Admin = () => {
-   
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
 
     return ( 
         <div>
             <Header/>
             <div className='outest-div'>
                 <a className='title'>Manage Products</a>
-                <div>
-                    <a>Main Products</a>
-                    <a>Inventory</a>
+                <div className='menu-tabs'>
+                    <div className={classes.root} >
+                        <div className={classes.demo1}>
+                            <AntTabs value={value} onChange={handleChange} aria-label="ant example">
+                            <AntTab label="Main Products" />
+                            <AntTab label="Inventory" />
+                            </AntTabs>
+                            <Typography className={classes.padding} />
+                        </div>
+                    </div>
                 </div>
-                <div style={{paddingTop:20}}>
+                <div style={{paddingTop:10}}>
                     <Table>
                         <thead>
                             <tr>
