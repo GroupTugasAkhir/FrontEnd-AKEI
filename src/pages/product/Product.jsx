17ab2becd122cf1b8563461363403fe56f9c6f64
hasianamin <hasianamin14@gmail.com>
2020-12-04 18:49:22 +0700

    get data from backend for landing page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardCustom from '../../component/card/CardCustom';
 import './style.css'
+import Axios from 'axios'
+import {API_URL_SQL} from './../../helpers/apiurl'
+import {priceFormatter} from './../../helpers/priceFormatter'
 
 const Product=()=> {
+
+    const [categories,setCategories]=useState(null)
+    const [catalog,setCatalog]=useState(null)
+
+    useEffect(()=>{
+        Axios.get(`${API_URL_SQL}/admin/category`)
+        .then((res)=>{
+            setCategories(res.data)
+        }).catch((err)=>console.log(err))
+        Axios.get(`${API_URL_SQL}/admin/getproductbypage/1`)
+        .then((res)=>{
+            setCatalog(res.data)
+        }).catch((err)=>console.log(err))
+
+    },[])
+
+    const renderCategory=()=>{
+        return categories.map((val,index)=>{
+            return (
+                <li>{val.category_name}</li>
+            )
+        })
+    }
+
+    if(categories===null || catalog===null){
+        return (
+            <div>Loading</div>
+        )
+    }
+
     return (
         <div className="section-product" id="product">
             <div className="title-text center-text">
@@ -12,10 +45,7 @@ const Product=()=> {
                 <div className="nav-category">
                     <ul>
                         <li className='active'>All</li>
-                        <li>Bed</li>
-                        <li>Sofa</li>
-                        <li>Chair</li>
-                        <li>Light</li>
+                        {renderCategory()}
                     </ul>
                 </div>
             </div>
