7e059684326f4e20aaf99d94363a1d86370ebff1
anggi <anggiprastianto30@gmail.com>
2020-12-09 12:16:12 +0700

    update ActivityLog

--- src/pages/admin/branch_activity.jsx
+++ src/pages/admin/branch_activity.jsx
@@ -7,18 +7,54 @@ import Axios from 'axios';
 
 const ActivityLog = (props) => {
     const [trackActivityLog, settrackActivityLog] = useState([])
+    const [page, setpage] = useState(1)
+    const [countPage, setcountPage] = useState('')
 
     useEffect(()=> {
         fetchData()
     },[])
 
+    useEffect(()=> {
+        fetchData()
+    },[page])
+
     const fetchData = () => {
-        Axios.get(`${API_URL_SQL}/admin/getWHActivityLog?userLoc=${props.Auth.notes}`)
+        Axios.get(`${API_URL_SQL}/admin/getWHActivityLog?userLoc=${props.Auth.notes}&page=${page}`)
         .then((res)=> {
-            settrackActivityLog(res.data)
+            settrackActivityLog(res.data.activityRes)
+            setcountPage(res.data.countAct[0].amountofact)
         }).catch(err=> {
             console.log(err);
         })
+    }
+
+    const movePage = (value) => {
+        setpage(value)
+    }
+
+    const renderPage = () => {
+        let amountPage = Math.ceil(countPage/5)
+        let arr = new Array(amountPage)
+        for(let i=0; i<arr.length; i++) {
+            if((i+1)===page) {
+                arr[i] = (
+                    <PaginationItem key={i} disabled>
+                        <PaginationLink >
+                            {i+1}
+                        </PaginationLink>
+                    </PaginationItem>
+                )
+            } else {
+                arr[i] = (
+                    <PaginationItem key={i} onClick={()=>movePage(i+1)} >
+                        <PaginationLink >
+                            {i+1}
+                        </PaginationLink>
+                    </PaginationItem>
+                )
+            }
+        }
+        return arr
     }
 
     const renderTableActivity=()=>{
@@ -26,11 +62,10 @@ const ActivityLog = (props) => {
           <tr key={index}>
             <th style={{display:'flex', justifyContent:'center', alignItems:'center'}}>{index+1}</th>
             <td>{val.product_name}</td>
-            <td>{val.trxqty}</td>
+            <td>{val.quantity}</td>
             <td>{dateFormatter(parseInt(val.date_in))}</td>
-            <td>{val.fromLoc}</td>
-            <td>{val.destLoc}</td>
             <td>{val.status}</td>
+            <td>{val.notes}</td>
           </tr>
         ))
     }
@@ -44,9 +79,8 @@ const ActivityLog = (props) => {
                         <th>Product</th>
                         <th>Quantity</th>
                         <th>Date</th>
-                        <th>From</th>
-                        <th>Destination</th>
-                        <th style={{width:300}}>Status</th>
+                        <th>Status</th>
+                        <th>Notes</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -55,7 +89,7 @@ const ActivityLog = (props) => {
             </Table>
             <div className='table_footer d-flex justify-content-center align-items-center'>
                 <Pagination aria-label="Page navigation example">
-                    {/* {renderPage()} */}
+                    {renderPage()}
                 </Pagination>
             </div>
         </div>
