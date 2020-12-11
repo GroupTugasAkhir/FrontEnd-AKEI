d3d164226537f52b45df61fb01d9c80821f60334
anggi <anggiprastianto30@gmail.com>
2020-12-11 19:24:19 +0700

    update and fixing bugs

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import DetailProduct from './pages/detail_product/DetailProduct'
 import Testing from './pages/Testing'
 import Admin from './pages/admin/admin'
 import UserProfile from './pages/user_profile/UserProfile'
+import Loading from './component/Loading'
 import 'bootstrap/dist/css/bootstrap.css';
 
 import {connect} from 'react-redux'
@@ -25,9 +26,7 @@ function App(props) {
 
   if(props.Auth.isLoading) {
     return (
-      <div>
-        LOADING
-      </div>
+      <Loading />
     )
   }
 
