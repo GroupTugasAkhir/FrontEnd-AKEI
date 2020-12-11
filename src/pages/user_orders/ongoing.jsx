e47194fa506ae171f96b18f4ac285fdf0ab115a8
Hannah <hannah4669@users.noreply.github.com>
2020-12-11 15:44:02 +0700

    aa

--- src/pages/user_orders/ongoing.jsx
+++ src/pages/user_orders/ongoing.jsx
@@ -6,6 +6,8 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import StarRatings from 'react-star-ratings';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -25,38 +27,79 @@ table: 'https://images.unsplash.com/photo-1602009445825-70e98455ea7c?ixid=MXwxMj
 
 const OnGoing = () => {
     const classes = useStyles();
+    const [modal, setModal] = useState(false);
+    const [rating, setRating] = useState(0)
+
+    const toggle = () => setModal(!modal);
+
+    const changeRating=(newRating, name)=>{
+        console.log(newRating)
+        setRating(newRating)
+        // console.log(rating)
+    }
 
     return ( 
-        <div className='order-box'>
-            <div className='order-content'>
-                <div className='order-top'>
+        <div>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Rate our product</ModalHeader>
+                <ModalBody>
                     <div>
-                        <img width={80} height={80} style={{borderRadius:10}} src={uriPic.chair} alt="AKEI"/>
+                        <StarRatings
+                            rating={rating}
+                            starRatedColor="red"
+                            changeRating={changeRating}
+                            numberOfStars={5}
+                            name='rating'
+                            starDimension='30px'
+                        />
+                        <textarea className='form-control mt-2' cols='30' rows='7' placeholder='Write your comment'></textarea>
+
                     </div>
-                    <div className='order-detail pl-3'>
-                        <div className='order-name' style={{fontSize:18}}>
-                            Chairrr
+                </ModalBody>
+                <ModalFooter>
+                    <button className="complete-button" >Complete</button>{' '}
+                    <button className="cancel-button" onClick={toggle}>Cancel</button>
+                </ModalFooter>
+            </Modal>
+            <div className='order-box'>
+                <div className='order-header'>
+                    <div style={{fontSize:22, fontWeight:500, color:' #72ceb8'}}>
+                        Delivered
+                    </div>
+
+                </div>
+                <div className='order-content'>
+                    <div className='order-top'>
+                        <div>
+                            <img width={80} height={80} style={{borderRadius:10}} src={uriPic.chair} alt="AKEI"/>
                         </div>
-                        <div className='order-name'>
-                            x 5
+                        <div className='order-detail pl-3'>
+                            <div className='order-name' style={{fontSize:18}}>
+                                Chairrr
+                            </div>
+                            <div className='order-name'>
+                                x 5
+                            </div>
                         </div>
+                    </div>
+                    <div className='order-price' style={{fontSize:18}}>
+                        100000
                     </div>
                 </div>
-                <div className='order-price' style={{fontSize:18}}>
-                    100000
+                <div className='order-content-bottom-box'>
+                    <div className='order-bottom-txt'>
+                        <button onClick={toggle} className='complete-button'>complete</button>
+                    </div>
+                    <div className='order-content-bottom'>
+                        <div className='order-bottom-txt' style={{fontSize:19}}>
+                            Total pesanan:
+                        </div>
+                        <div className='order-bottom-txt ml-2' style={{fontSize:25}}>
+                            Rp 500.000
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='order-content-bottom-box'>
-                <div className='order-content-bottom'>
-                    <div>
-                        Total pesanan:
-                    </div>
-                    <div>
-                        Rp 500.000
-                    </div>
-                </div>
-            </div>
-            
         </div>
      );
 }
